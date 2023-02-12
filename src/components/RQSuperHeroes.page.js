57f115b8934b57c076825dc1a23e502455a0c155
Нікіта Яблочніков <sensei@Nikitas-MacBook-Air.local>
2023-02-12 21:21:48 +0200

    added: react-query package/implement fetching

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,3 +1,25 @@
+import {useQuery} from "react-query";
+import axios from "axios";
+
+const fetchSuperHeroes = () => {
+    return axios.get('https://localhost:4000')
+}
+
 export const RQSuperHeroesPage = () => {
-  return <h2>React Query Super Heroes Page</h2>
+  const { isLoading, data } = useQuery('super-heroes', () => {
+    return axios.get('http://localhost:4000/superheroes');
+  })
+
+  if( isLoading ) {
+    return  <h2>Loading...</h2>
+  }
+
+  return (
+      <>
+        <h2>React Query Super Heroes Page</h2>
+          {data && data.data.map(hero => {
+              return <div key={hero.id}>{hero.name}</div>
+          })}
+      </>
+  )
 }
