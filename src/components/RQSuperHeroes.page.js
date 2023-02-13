cc205d5cec16736304b98a6285c139b24af0c14a
Нікіта Яблочніков <sensei@Nikitas-MacBook-Air.local>
2023-02-13 02:16:16 +0200

    feat: onSuccess/onErrors callbacks+poling data

--- src/components/RQSuperHeroes.page.js
+++ src/components/RQSuperHeroes.page.js
@@ -1,13 +1,31 @@
 import {useQuery} from "react-query";
 import axios from "axios";
+import {useState} from "react";
 
 const fetchSuperHeroes = () => {
     return axios.get('http://localhost:4000/superheroes')
 }
 
+const addHero = async () => {
+    await axios.post('http://localhost:4000/superheroes', { id: 4, name: 'Iron Man', alterEgo: 'Tony Stark'})
+}
+
 export const RQSuperHeroesPage = () => {
-  const { isLoading, isFetching, data, error, isError, refetch } = useQuery('super-heroes', fetchSuperHeroes,{
-      enabled: false,
+    const [fetchInterval, setFetchInterval] = useState(3000)
+    const onSuccessfulFetch = (data) => {
+        console.log('Perform side effect on successful fetch', data);
+        data?.data.length === 4 && setFetchInterval(false);
+    }
+
+    const onFailedFetch = (error) => {
+        console.log('Perform side effect on failed fetch', error)
+    }
+
+
+    const { isLoading, isFetching, data, error, isError, refetch } = useQuery('super-heroes', fetchSuperHeroes,{
+        onSuccess: onSuccessfulFetch,
+        onError: onFailedFetch,
+        refetchInterval: data =>  data?.data.length > 3 ? false : 2000,
   })
 
   if( isLoading || isFetching ) {
@@ -22,6 +40,7 @@ export const RQSuperHeroesPage = () => {
       <>
         <h2>React Query Super Heroes Page</h2>
           <button onClick={refetch}>Fetch heroes</button>
+          <button onClick={addHero}>Add hero</button>
           {data && data.data.map(hero => {
               return <div key={hero.id}>{hero.name}</div>
           })}
--- src/components/SuperHeroes.page.js
+++ src/components/SuperHeroes.page.js
@@ -7,7 +7,7 @@ export const SuperHeroesPage = () => {
   const [error, setError] = useState('null')
 
   useEffect(() => {
-    axios.get('http://localhost:4000/superheroes1').then(res => {
+    axios.get('http://localhost:4000/superheroes').then(res => {
       setData(res.data)
       setIsLoading(false)
     }).catch(err => {
